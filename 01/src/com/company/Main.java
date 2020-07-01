a3d06a1e9495ebbf2eb367cfe874d984bba536f1
mustafaiam <67704453+mustafaiam@users.noreply.github.com>
2020-07-01 20:46:56 +0300

    new file:   01/.idea/.gitignore 	new file:   01/.idea/description.html 	new file:   01/.idea/encodings.xml 	new file:   01/.idea/misc.xml 	new file:   01/.idea/modules.xml 	new file:   01/.idea/project-template.xml 	new file:   01/01.iml 	new file:   01/src/com/company/Main.java

---
+++ 01/src/com/company/Main.java
@@ -0,0 +1,14 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args)
+    {
+        // Java Program To Print An Integer (Entered By User)
+        Scanner scanner = new Scanner(System.in);
+        int input = Integer.parseInt(scanner.nextLine());
+        System.out.print(input);
+    }
+}
